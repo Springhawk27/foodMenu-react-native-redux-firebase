ae1bd049bca506b5592a2ffa5b50ef9d47ff0e0e
Springhawk27 <sajjad0mahmud@gmail.com>
2021-12-23 23:41:51 +0600

    react navigation installed and implemented

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import AppNavigator from './app/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <AppNavigator></AppNavigator>
+    <NavigationContainer>
+      <AppNavigator></AppNavigator>
+    </NavigationContainer>
   );
 }
 
