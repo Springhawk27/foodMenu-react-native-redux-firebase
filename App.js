970cc4967ffd3067bb24fa6e4763c30604c2193f
Springhawk27 <sajjad0mahmud@gmail.com>
2021-12-25 01:23:19 +0600

    redux and axios installed and implemented

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import AppNavigator from './app/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './app/redux/store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator></AppNavigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator></AppNavigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
